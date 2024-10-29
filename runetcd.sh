2cf7c5cc1edf9dd10a83186b7d156457a2dbdb70
MFMemon <mohammadfaizan91@gmail.com>
2024-10-30 01:26:22 +0400

    modified deployment script to setup etcd cluster

--- runetcd.sh
+++ runetcd.sh
@@ -1,5 +1,5 @@
 
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl iproute2
 
 ETCD_VER=v3.4.34
 
@@ -15,4 +15,11 @@ curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/
 tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
-/tmp/etcd-download-test/etcd
+listen_ip = $(ip a | awk "/inet/ && /brd/" | tail -1 | awk '{print $2}' | awk -F/ '{print $1}')
+
+/tmp/etcd-download-test/etcd --name $1 --initial-advertise-peer-urls http://$2 \
+  --listen-peer-urls http://${listen_ip}:$6 \
+  --listen-client-urls http://${listen_ip}:$7,http://127.0.0.1:$7 \
+  --advertise-client-urls http://$5 --initial-cluster-token etcd-cluster-1 \
+  --initial-cluster infra0=http://$2,infra1=http://$3,infra2=http://$4 \
+  --initial-cluster-state new
